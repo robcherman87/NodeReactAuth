5737c7745ce1a387c440d0afb20dea6c82f949ee
Rob Herman <robherman@Robs-MBP-2.domain>
2018-05-22 09:42:41 -0600

    Hooked up Mongoose and added info for MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-// Heroku link https://mysterious-basin-65094.herokuapp.com/
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -8,5 +11,4 @@ require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
 
